89e1b22282553fa8ed96f2b4201910db5de9b1f9
quentinrouger <quentin_rouger@hotmail.fr>
2023-05-15 04:08:55 -0700

    Add: The 2-binary_tree_insert_right.c file and add protoypes to the header file

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_insert_right - insert a node as the right-child of another node
+  * @parent: the pointer to the struc parent
+  * @value: the value to the new right node
+  * Return: the pointer to the new right node
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *right_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	right_node = malloc(sizeof(binary_tree_t));
+	if (right_node == NULL)
+		return (NULL);
+
+	right_node->parent = parent;
+	right_node->n = value;
+	right_node->right = parent->right;
+
+	if (parent->right != NULL)
+		parent->right->parent = right_node;
+	right_node->right = parent->right;
+
+	parent->right = right_node;
+
+	return (right_node);
+}
