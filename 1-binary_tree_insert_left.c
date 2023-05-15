39d2da3618bbac980acf3f6e791e1bbd99745242
MarcP70 <marcpourias@yahoo.fr>
2023-05-15 05:29:19 -0700

    Fix: call function binary_tree_node in file 1-binary_tree_insert_left.c to optimize the code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,16 +13,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	left_node = malloc(sizeof(binary_tree_t));
+	left_node = binary_tree_node(parent, value);
 	if (left_node == NULL)
 		return (NULL);
 
-	left_node->parent = parent;
-	left_node->n = value;
-	left_node->left = parent->left;
-
 	if (parent->left != NULL)
-		parent->left->parent = left_node;
+	{
+		left_node->left = parent->left;
+		left_node->left->parent = left_node;
+	}
 
 	parent->left = left_node;
 
