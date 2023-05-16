cdae6634f69c8c7af1a986bf76e742ff110f78de
quentinrouger <quentin_rouger@hotmail.fr>
2023-05-16 03:36:30 -0700

    Fix: removed if conditions because not necessary

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,10 +13,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
 		left_height = binary_tree_height(tree->left) + 1;
 
-	if (tree->right != NULL)
 		right_height = binary_tree_height(tree->right) + 1;
 
 	return (left_height > right_height ? left_height : right_height);
