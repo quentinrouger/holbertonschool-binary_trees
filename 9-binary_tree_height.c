02e3c230288061e8ce617ba54260be62caaba42a
quentinrouger <quentin_rouger@hotmail.fr>
2023-05-16 03:56:56 -0700

    Fix: problem with the if condition

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,8 +13,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left != NULL)
+		left_height = binary_tree_height(tree->left) + 1;
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	if (tree->right != NULL)
+		right_height = binary_tree_height(tree->right) + 1;
+
+	return (left_height > right_height ? left_height : right_height);
 }
